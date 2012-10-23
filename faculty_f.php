a20237efce13ecfd20adfe92f49654e12a5a40ae
apoorv10020@iiitd.ac.in <apoorv10020@iiitd.ac.in@fa5ca77a-1bb9-e9fb-0bc3-d9dc457fcce3>
2012-10-23 11:43:40 +0000

    Editing leave application

--- faculty_f.php
+++ faculty_f.php
@@ -1,4 +1,4 @@
-<? include('cxn.php');
+<?php include('cxn.php');
 session_start();
 ?>
 <div class="leave-body">
@@ -8,7 +8,8 @@ session_start();
 
 <div class="l-notification">
 
-<?
+<?php
+$status=0;
 $id=$_SESSION['userid'];
 $qr2="select * from `leave` where (id='$id') order by time DESC LIMIT 0,10 ";
 $r1=mysql_query($qr2);
@@ -17,27 +18,32 @@ echo mysql_error();
 	while($nt=mysql_fetch_array($r1))
 	{
 	?>
-   <div id="l-body"> <b>Leave from <? echo $nt['start'];?> to <? echo $nt['end']; ?><br/>
-    for <? echo $nt['no_days'];?> days</b><br/>
-    <b> Type: </b> <? echo $nt['type']; ?><br/>
-    Reason being "<? echo $nt['reason']; ?>" <br/>
-    <?
+   <div id="l-body"> <b>Leave from <?php echo $nt['start'];?> to <?php echo $nt['end']; ?> (applied on <?php echo $nt['time']; ?>)<br/>
+    for <?php echo $nt['no_days'];?> days</b><br/>
+    <b> Type: </b> <?php echo $nt['type']; ?><br/>
+    Reason being "<?php echo $nt['reason']; ?>" <br/>
+     <?php
+	
 	if ($nt['comment_a']!='')
 	{
 		?>
-        Admin's comment: <? echo $nt['comment_a'];?>
-        <?
+      <br/><b>  Admin's comment:</b> <?php echo $nt['comment_a'];?>
+        <?php
 	}
 	if ($nt['comment_d']!='')
 	{
 		?>
-       Director's comment: <? echo $nt['comment_d'];?>
-        <?
+     <br/><b> Director's comment:</b><?php echo $nt['comment_d'];?>
+        <?php
 	}
-	
+	if ($nt['status']==4)
+	{ ?>
+    <br/><a data-toggle="modal"  href="#modify<?php echo $nt['l_id']; ?>" title="Modify Leave">Edit your leave</a>
+    <?php
+	}
     ?>
     </div>
-    <div class="<? if ($nt['status']==0) {echo "pending";
+    <div class="<?php if ($nt['status']==0) {echo "pending";
 	}
 	else if ($nt['status']==1)
 	{
@@ -53,10 +59,11 @@ echo mysql_error();
 	}
 	else if ($nt['status']==4)
 	{
+		$status=4;
 	echo "sentback";	
 	}
 	?>"></div>
-    <?   
+    <?php   
 	}
 ?>
 
@@ -69,19 +76,37 @@ echo mysql_error();
 
 <div class="counter">
 <div class="counter_con">
-<h2>Leave Counter </h2><br/>
-<?
+<center><h2>Leave Counter </h2></center><br/>
+<center>(<em>w.e.f</em> 1st April 2012 - 31st March 2013)</center>
+<?php
 $qry4 = "SELECT * FROM counter WHERE id='$id'";
   $result2 = mysql_query($qry4);
    $info2=mysql_fetch_assoc($result2);
   // $casual=8-$info2['casual'];
    ?>
-   
-   <b>Paid leaves:</b> <? echo " ". $info2['paid']." "; ?> remaining <br/>
-   <b>Work leaves:</b> <? echo " ". 0+$info2['work']." "; ?> taken<br/>
-   <b>Unpaid leaves:</b><? echo " ". 0+$info2['unpaid']." "; ?> taken <br/>
-   <b>Vacation leaves:</b><? echo " ". 0+$info2['vacation']." "; ?> taken <br/>
-   <b>Casual leaves:</b><? echo " ". $info2['casual']." "; ?> remaining <br/>
+   <table class="table ">
+  <thead>
+    <tr>
+      <th>Types</th>
+       <th>Entitled</th>
+      <th> Taken</th>
+      <th>Remaining</th>
+     
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td> <b>Paid leaves*</b></td><td>30</td><td><?php echo " ". 30-$info2['paid']." "; ?></td><td><?php echo " ". $info2['paid']." "; ?></td></tr>
+     <tr> <td><b>Work leaves</b></td><td>N.A.</td><td><?php echo " ". 0+$info2['work']." "; ?></td><td>N.A.</td></tr>
+     <tr> <td><b>Unpaid leaves</b></td><td>N.A.</td><td><?php echo " ". 0+$info2['unpaid']." "; ?></td><td>N.A.</td></tr>
+     <tr> <td> <b>Vacation leaves</b></td><td>N.A.</td><td><?php echo " ". 0+$info2['vacation']." "; ?></td><td>N.A.</td></tr>
+    <tr>  <td> <b>Casual leaves</b></td><td>8</td><td><?php echo " ". 8-$info2['casual']." "; ?></td><td><?php echo " ". $info2['casual']." "; ?></td>
+    
+    </tr>
+  </tbody>
+  
+</table>
+ <center> *carry forward to next year</center>
    
 </div>
 </div>
