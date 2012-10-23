4d2bc9d88548f6e4db3df244b78e5fe0dcc59ec3
apoorv10020@iiitd.ac.in <apoorv10020@iiitd.ac.in@fa5ca77a-1bb9-e9fb-0bc3-d9dc457fcce3>
2012-10-23 11:42:13 +0000

    Edit info

---
+++ editinfo.php
@@ -0,0 +1,16 @@
+<?php
+include("cxn.php");
+session_start();
+
+$id=$_SESSION['userid'];
+
+$name=$_POST['name'];
+$address=$_POST['address'];
+$phone=$_POST['phone'];
+$m_status=$_POST['m_status'];
+$child=$_POST['child'];
+
+//echo $name.$address.$phone.$child;
+mysql_query("UPDATE users SET name='$name', address='$address', m_status='$m_status', phone='$phone', child='$child'  where id='$id'") ; 
+header ('LOCATION:'.$_SERVER['HTTP_REFERER']);
+?>
