2495f40fb24310ceb7dc520ab2b5ceb5e5e0c1c2
apoorv10020@iiitd.ac.in <apoorv10020@iiitd.ac.in@fa5ca77a-1bb9-e9fb-0bc3-d9dc457fcce3>
2012-10-23 11:51:24 +0000

    Upload dp and Modify leave

--- faculty.php
+++ faculty.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 include_once('cxn.php');
 
@@ -96,6 +96,47 @@ var iWeeks, iDateDiff, iAdjust = 0;
  //document.getElementById('nod').innerHTML = iDateDiff + 1;
  document.getElementById('nod').value =iDateDiff + 1;
  }
+ 
+ function weekdayc2()
+ {
+Date3 = document.getElementById('Date3').value;
+Date4 = document.getElementById('Date4').value;
+alert (Date3);
+dDate1=new Date (Date3);
+dDate2=new Date(Date4);
+//document.getElementById('nod').value =date1;
+//calcBusinessDays(date1, date2);
+var iWeeks, iDateDiff, iAdjust = 0;
+
+  if (dDate2 < dDate1) return -1;                 // error code if dates transposed
+
+  var iWeekday1 = dDate1.getDay();                // day of week
+  var iWeekday2 = dDate2.getDay();
+
+  iWeekday1 = (iWeekday1 == 0) ? 7 : iWeekday1;   // change Sunday from 0 to 7
+  iWeekday2 = (iWeekday2 == 0) ? 7 : iWeekday2;
+
+  if ((iWeekday1 > 5) && (iWeekday2 > 5)) iAdjust = 1;  // adjustment if both days on weekend
+
+  iWeekday1 = (iWeekday1 > 5) ? 5 : iWeekday1;    // only count weekdays
+  iWeekday2 = (iWeekday2 > 5) ? 5 : iWeekday2;
+
+  // calculate differnece in weeks (1000mS * 60sec * 60min * 24hrs * 7 days = 604800000)
+  iWeeks = Math.floor((dDate2.getTime() - dDate1.getTime()) / 604800000)
+
+  if (iWeekday1 <= iWeekday2) {
+    iDateDiff = (iWeeks * 5) + (iWeekday2 - iWeekday1)
+  } else {
+    iDateDiff = ((iWeeks + 1) * 5) - (iWeekday1 - iWeekday2)
+  }
+
+  iDateDiff -= iAdjust                            // take into account both days on weekend
+
+ // return (iDateDiff + 1);                         // add 1 because dates are inclusive
+ 
+ //document.getElementById('nod').innerHTML = iDateDiff + 1;
+ document.getElementById('nod2').value =iDateDiff + 1;
+ }
 </script>
 <script>
 
@@ -149,31 +190,34 @@ setInterval( "update()", 5000 );
 <div id="rim"><a href="#"><b>Reimbursements</b></a></div>
 <div id="bills"><a href="#"><b>Bills</b></a></div>-->
 <div id="logout_a"><a href="logout.php"><b>Logout</b></a></div>
-<?
+<?php
 $id=$_SESSION['userid']; 
 $qry = "SELECT * FROM users WHERE id='$id'";
   $result = mysql_query($qry);
    $info=mysql_fetch_assoc($result);?>
 <div id="logo2"></div></div>
 <div class="info_bar">
-<div id="name" style="font-family:'Kohinoor Bold';"><? echo $info['name']; ?> <br/><br/> <? echo "<div style='font-size:16px;'>".$info['role']." </div>"; ?></div>
+<div id="name" style="font-family:'Kohinoor Bold';"><?php echo $info['name']; ?> <br/><br/> <?php echo "<div style='font-size:16px;'>".$info['role']." </div>"; ?></div>
 <div class="dp_area">
 <a data-toggle="modal"  href="#addpic" title="Click to add/change your pic">
-<? if(!$info['pic'])
+<?php if(!$info['pic'])
 {?>
-<img src="pics/dp.png"/>
+<img src="pics/dp.png"  style=" position:relative; top:6px;"/>
 
-<? }
+<?php }
 else
 {
-	echo "<img src='pics/".$info['pic']."' />";
+	echo "<img src='pics/".$info['pic']."' height='150' width='150'/>";
 }?>
 </a>
 </div>
 <a data-toggle="modal"  href="#infoEdit" title="Change your info">
 <div id="info">
-
-<? echo $info['address']."<br/>".$info['phone']."<br/>"; ?>
+<div class="icon-address"></div>
+<?php echo $info['address'];?>
+<br/>
+<div class="icon-phone"></div>
+<?php echo $info['phone']; ?>
 
 </div></a>
 </div>
@@ -213,7 +257,7 @@ Number of Classes Missed<br/>
 Source of Financial Support<br/>
 <select name='finsup' id="finsup"  value='' onchange='Checksource(this.value);'><option value="Institute">Institute</option><option value="PDA">PDA</option><option value="Project Number">Project Number</option><option value="Other">Other</option></select><br/>
 
-Attach a file: <input type="file" name="file" value="">
+Attach a file: <input type="file" name="file" value=""><br/>
 Approximate expenses for the travel<br/>
 <textarea name='amount' rows='1' value=""></textarea><br/>
 <div id="cb" style="display:none;">
@@ -221,7 +265,8 @@ Approximate expenses for the travel<br/>
 </div>
 Destination<br/>
 <textarea name="city" rows="1" placeholder="City"></textarea><textarea name="country" rows="1" placeholder="Country"></textarea><br/>
-
+Arrangement of classes missed: <br/>
+<textarea name="arrange" rows="3" ></textarea><br/>
 
 
 
@@ -242,7 +287,7 @@ Destination<br/>
             
             <div class="modal-body">
               <center><p><b>Add/Change your photo</b> </p></center>
- <form  action="addpic.php" method="post">
+ <form  action="addpic.php" method="post"  enctype="multipart/form-data" >
        <input name="photo" type="file" size="10" />
             <button type="submit" class="btn btn-primary btn-info " name="submit" value="Login" style="cursor:pointer">Add this photo </button>
 </form>
@@ -258,20 +303,20 @@ Destination<br/>
               <center><p><b>Edit Info</b> </p></center>
  <form  action="editinfo.php" method="post">
 Name <br/>
-<textarea  id="expand" class="txtarea" name='name' rows='1' cols='1000' wrap="physical" ><? echo $info['name'];?></textarea> <br/>
+<textarea  id="expand" class="txtarea" name='name' rows='1' cols='1000' wrap="physical" ><?php echo $info['name'];?></textarea> <br/>
 Address <br/>
-  <textarea  id="expand" class="txtarea" name='address' rows='5' cols='1000' wrap="physical" ><? echo $info['address'];?></textarea> <br/>
+  <textarea  id="expand" class="txtarea" name='address' rows='5' cols='1000' wrap="physical" ><?php echo $info['address'];?></textarea> <br/>
   Phone No. <br/>
-  <textarea  id="expand" class="txtarea" name='phone' rows='1' cols='1000' wrap="physical" ><? echo $info['phone'];?></textarea> <br/>
+  <textarea  id="expand" class="txtarea" name='phone' rows='1' cols='1000' wrap="physical" ><?php echo $info['phone'];?></textarea> <br/>
  Marital Status <br/>
   <select name="m_status">
  
   <option>
-   <?  echo $info['m_status']; ?>
+   <?php  echo $info['m_status']; ?>
   </option>
   
   <option>
-  <?  if($info['m_status']=="married"){ 
+  <?php  if($info['m_status']=="married"){ 
    echo "single"; }
   else if($info['m_status']=="single")
   echo "married"; ?>
@@ -279,7 +324,7 @@ Address <br/>
  </select>
   <br/>
   No. of children <br/>
-  <textarea  id="expand" class="txtarea" name='child' rows='1' cols='1000' wrap="physical" ><? echo $info['child'];?></textarea> <br/>
+  <textarea  id="expand" class="txtarea" name='child' rows='1' cols='1000' wrap="physical" ><?php echo $info['child'];?></textarea> <br/>
   <button type="submit" class="btn btn-primary btn-info " name="submit" value="Login" style="cursor:pointer"> Save Changes </button>
   <br/><a href="editpass.php">Change your password</a>
 </form>
@@ -288,6 +333,75 @@ Address <br/>
 
           </div>
           </div>
+          
+          
+       
+       <?php
+	   
+	   $qr2="select * from `leave` where id='$id'";
+$r1=mysql_query($qr2);
+echo mysql_error();    
+	$cnt=0;
+	while($nt=mysql_fetch_array($r1))
+	{
+		$lid=$nt['l_id'];
+		//$cnt++;?>
+         <div id="modify<?php echo $lid; ?>" class="modal hide fade" style="display: none; ">
+            
+              <button class="close" data-dismiss="modal">×</button>
+           
+            
+            <div class="modal-body">
+              <center><p><b>Modify/Edit Leave</b> </p></center>
+ <form enctype="multipart/form-data" action="modleave.php?lid=<?php echo $lid; ?>" method="post">
+From (mm/dd/yy):<br/>
+<!-- <textarea id="inputDate"  name='from' rows='1' cols='500' wrap="physical" placeholder="12/12/12" ></textarea>-->
+ <input class="d3" id="Date3" name="from" type="date" value="" > <br/>
+To (mm/dd/yy):<br/>
+ <input class="d4" id="Date4" name="to" type="date" value="" onChange="weekdayc2();" > <br/> 
+ 
+No. of days <br/>
+<input  id="nod2"  name='days' rows='1' cols='1000' wrap="physical" value="" /> <br/>
+Type of leave <br/>
+<select name="type" id="type" onchange='Checkfin(this.value);'> <option value="Paid Leave">Paid Leave</option> <option  value="Work Leave">Work Leave</option> <option value="Unpaid Leave">Unpaid Leave</option> <option value="Vacation Leave">Vacation Leave</option> <option value="Casual Leave">Casual Leave</option><option value="Sabbatical Leave">Sabbatical Leave</option></select><br/>
+
+Reason<br/>
+<textarea name="reason" rows="3"><?php echo $nt['reason']; ?></textarea><br/>
+Number of Classes Missed<br/>
+<textarea name="n_cls" rows="1"><?php echo $nt['c_miss']; ?></textarea><br/>
+
+<div id="fin" style="display:none;">
+Source of Financial Support<br/>
+<select name='finsup' id="finsup"  value='' onchange='Checksource(this.value);'><option value="Institute">Institute</option><option value="PDA">PDA</option><option value="Project Number">Project Number</option><option value="Other">Other</option></select><br/>
+
+Attach a file: <input type="file" name="file" value=""><br/>
+Approximate expenses for the travel<br/>
+<textarea name='amount' rows='1' value=""></textarea><br/>
+<div id="cb" style="display:none;">
+<input type='checkbox' name='agree' value='1'  /> I have checked that in my PDA/Project there are sufficient funds under suitable budget head for this travel.</div><br />
+</div>
+Destination<br/>
+<textarea name="city" rows="1" placeholder="City"><?php echo $nt['city']; ?></textarea><textarea name="country" rows="1" placeholder="Country"><?php echo $nt['country']; ?></textarea><br/>
+Arrangement of classes missed: <br/>
+<textarea name="arrange" rows="3" ><?php echo $nt['arrangement']; ?></textarea><br/>
+
+
+
+
+  <button type="submit" class="btn btn-primary btn-info " name="submit" value="Login" style="cursor:pointer"> Re-submit </button>
+  
+</form>
+           
+          </div>
+          </div>
+
+        
+        <?php
+	}
+	
+	?>           
+          
+          
 
 
       <script type="text/javascript" src="js/bootstrap-button.js"></script>
@@ -301,7 +415,9 @@ Address <br/>
   
       if ( elem.type === 'text' ) {  
          $('#date').datepicker(); 
-		 $('#date2').datepicker();   
+		 $('#date2').datepicker(); 
+		 $('#date3').datepicker(); 
+		 $('#date4').datepicker();   
       }  
    })();  
   
@@ -313,7 +429,7 @@ Address <br/>
 
 </body>
 </html>
-<? }
+<?php }
 else if ($_SESSION['role']=="admin")
 {
 	header('LOCATION:admin.php');
