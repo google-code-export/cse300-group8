1a5008c2b97c33ec8be0d98ecca79e97f148e466
apoorv10020@iiitd.ac.in <apoorv10020@iiitd.ac.in@fa5ca77a-1bb9-e9fb-0bc3-d9dc457fcce3>
2012-12-13 20:50:14 +0000

    removed editing name

--- trunk/editinfo.php
+++ trunk/editinfo.php
@@ -4,13 +4,13 @@ session_start();
 
 $id=$_SESSION['userid'];
 
-$name=$_POST['name'];
+//$name=$_POST['name'];
 $address=$_POST['address'];
 $phone=$_POST['phone'];
 $m_status=$_POST['m_status'];
 $child=$_POST['child'];
 
 //echo $name.$address.$phone.$child;
-mysql_query("UPDATE users SET name='$name', address='$address', m_status='$m_status', phone='$phone', child='$child'  where id='$id'") ; 
+mysql_query("UPDATE users SET  address='$address', m_status='$m_status', phone='$phone', child='$child'  where id='$id'") ; 
 header ('LOCATION:'.$_SERVER['HTTP_REFERER']);
 ?>
